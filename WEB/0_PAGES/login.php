d312deadd90728c4c4a91deff53e4bd7ae0cb4be
YA2SINEE <113427389+YA2SINEE@users.noreply.github.com>
2022-11-29 23:09:15 +0100

    add

--- WEB/0_PAGES/login.php
+++ WEB/0_PAGES/login.php
@@ -2,14 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" href="../logoBricolage.png" />
-    	
-    <link rel="stylesheet" href="../1_css/css_main.css" />
-    <link rel="stylesheet" href="../1_css/css_login.css" />
+    <link rel="icon" href="../logoBricolage.png"/>
 
+    <link rel="stylesheet" href="../1_css/css_main.css"/>
+    <link rel="stylesheet" href="../1_css/css_login.css"/>
 
 
     <title> Shop Theo & Yassine </title>
@@ -18,74 +17,74 @@
 
 <div class="header">
 
-	<a href="../index.php">
-	<img class="logo" src="../logoBricolage.png" alt=" LOGO  ">
-	</a>
+    <a href="../index.php">
+        <img class="logo" src="../logoBricolage.png" alt=" LOGO  ">
+    </a>
 
-	<div class="banniere">
-	  <h1> Shop Theo & Yassine </h1>
-	</div>
-
-
-	<div id="purchase_in" class="menu">
-		<a href="#">CONNEXION</a>
-	</div>
-	<div class="purchase_border">
-	</div>
+    <div class="banniere">
+        <h1> Shop Theo & Yassine </h1>
+    </div>
 
 
-	<div id="modeles" class="menu">
-		<a href="modeles.html">Panier</a> <!-- AJOUTER REDIRECTION PANIER PAR LA SUITE-->
-	</div>
+    <div id="purchase_in" class="menu">
+        <a href="#">CONNEXION</a>
+    </div>
+    <div class="purchase_border">
+    </div>
+
+
+    <div id="modeles" class="menu">
+        <a href="modeles.html">Panier</a> <!-- AJOUTER REDIRECTION PANIER PAR LA SUITE-->
+    </div>
 
 </div>
 <!-- -------------------------------------------------------------- -->
 
 <div class="page">
 
-	<img src="../2_images/DIAPO/Levante_1.jpg"/>  <!-- Mettre une autre image jolie pour backgorund -->
+    <img src="../2_images/DIAPO/Levante_1.jpg"/>  <!-- Mettre une autre image jolie pour backgorund -->
 
-<div class="contenu">
+    <div class="contenu">
 
-<form method="post" action="verification.php">
-  <fieldset class="mainBord">
-		 <legend>Connexion</legend>
-	  <br>
-		 
-		 <label for="nom">Nom Client</label> </br>
-		 <input type="text" class="email-bt"  id="nom" placeholder="Nom Client" name="username" required/>
-		</br>
+        <form method="post" action="../5_PHP/verification.php">
+            <fieldset class="mainBord">
+                <legend>Connexion</legend>
+                <br>
 
-		 <label for="mdp">Mots de Passe</label>  <!--  Mettre le id mots de passe si ca change rien au code -->
-		 <input type="password" class="email-bt"  id="mdp" placeholder="Mots de Passe" name="password" required/>
+                <label for="nom">Nom Client</label> </br>
+                <input type="text" class="email-bt" id="nom" placeholder="Nom Client" name="username" required/>
+                </br>
+
+                <label for="mdp">Mots de Passe</label>  <!--  Mettre le id mots de passe si ca change rien au code -->
+                <input type="password" class="email-bt" id="mdp" placeholder="Mots de Passe" name="password" required/>
 
 
-		<input class="send_bt" type="submit" id="submit"  value="LOGIN" />
-	  <br>
-		<input class="call_text" type="submit"  value="S'inscire"/>
-	  <?php
-                if(isset($_GET['erreur'])){
+                <input class="send_bt" type="submit" id="submit" value="LOGIN"/>
+                <br>
+                <input class="call_text" type="submit" value="S'inscire"/>
+                <?php
+                if (isset($_GET['erreur'])) {
                     $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
+                    if ($err == 1 || $err == 2)
                         echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-	  }
-	  ?>
+                }
+                ?>
 
-	</fieldset>
+            </fieldset>
 
-</div>
+    </div>
 
-<div class="footer">
+    <div class="footer">
 
-<ul>
-  <li>LebegueKaddouri © 2022</li>
-  <li><a href="conseils.pdf"> Conseils d'utilisation</a></li>
-	<li><a href="0_PAGES/contact.html">Panier</a></li>   <!-- A MODIFIER ET METTRE LE PATH DE PANIER -->
-	<li><a href="0_PAGES/contact.html">Produits</a></li>  <!-- A MODIFIER ET METTRE LE PATH DE Produit -->
-</ul>
+        <ul>
+            <li>LebegueKaddouri © 2022</li>
+            <li><a href="conseils.pdf"> Conseils d'utilisation</a></li>
+            <li><a href="0_PAGES/contact.html">Panier</a></li>   <!-- A MODIFIER ET METTRE LE PATH DE PANIER -->
+            <li><a href="0_PAGES/contact.html">Produits</a></li>  <!-- A MODIFIER ET METTRE LE PATH DE Produit -->
+        </ul>
 
 
-</div>
+    </div>
 </div>
 </body>
 </html>

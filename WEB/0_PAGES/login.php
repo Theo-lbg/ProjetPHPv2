49d09d85b5b4ed418510d8f3857cd7e851eb33d8
Theo-lbg <theo.lebegue@uphf.fr>
2022-12-16 12:03:43 +0100

    Add all things

--- WEB/0_PAGES/login.php
+++ WEB/0_PAGES/login.php
@@ -1,4 +1,35 @@
 <!DOCTYPE html>
+<?php
+// Démarrer la session
+session_start();
+// Vérifier si le formulaire a été soumis
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Récupérer les données du formulaire
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Vérifier les informations de connexion de l'utilisateur
+    $pdo = new PDO('mysql:host=localhost;dbname=bdd_projetphpty', 'adminty', 'adminty');
+    $stmt = $pdo->prepare('SELECT * FROM Client WHERE Nom_Client = ? AND Mdp = ?');
+  $stmt->execute([$username, $password]);
+  $user = $stmt->fetch();
+
+  if ($user) {
+    // Démarrer une session et enregistrer l'utilisateur
+    session_start();
+    $_SESSION['ID_Client'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+
+    // Rediriger l'utilisateur vers la page d'accueil
+    header('Location: ../index.php');
+    exit;
+  } else {
+    // Afficher un message d'erreur si les informations de connexion sont incorrectes en html
+    echo '<h1>Username or password is incorrect</h1>';
+
+  }
+}
+?>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
@@ -46,7 +77,7 @@
 
     <div class="contenu">
 
-        <form method="post" action="../5_PHP/verification.php">
+        <form method="post" action="">
             <fieldset class="mainBord">
                 <legend>Connexion: </legend>
                 <br>
@@ -58,13 +89,6 @@
                 <input class="send_bt" type="submit" id="submit" value="Se connecter"/>
                 <br>
                 <a href="enregistrement.php">S'inscrire</a>
-                <?php
-                if (isset($_GET['erreur'])) {
-                    $err = $_GET['erreur'];
-                    if ($err == 1 || $err == 2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
 
             </fieldset>
 
--- WEB/index.php
+++ WEB/index.php
@@ -1,29 +1,28 @@
 <!-- LEBEGUE Theo / KADDOURI_Yassine -->
 <?php
-session_start();
-$conn = mysqli_connect("localhost", "adminty", "adminty", "bdd_projetphpty");
 
-if(!empty($_SESSION["username"])){
-    $id = $_SESSION["username"];
-    $result = mysqli_query($conn, "SELECT * FROM client WHERE Nom_Client = $id");
-    $row = mysqli_fetch_assoc($result);
-}
-else{
-    header("Location: login.php");
+
+// Vérifier que la session a été démarrée
+if (session_status() == PHP_SESSION_ACTIVE) {
+    // Vérifier si la clé 'username' existe dans la session
+    if (isset($_SESSION['username'])) {
+        // Afficher le nom de la session
+        echo "Bienvenue, " . $_SESSION['username'];
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" href="logoBricolage.png" />
-    	
-    <link rel="stylesheet" href="../WEB/1_CSS/css_main.css" />
-    <link rel="stylesheet" href="../WEB/1_CSS/css_PAGE_1.css" />
+    <link rel="icon" href="logoBricolage.png"/>
 
+    <link rel="stylesheet" href="../WEB/1_CSS/css_main.css"/>
+    <link rel="stylesheet" href="../WEB/1_CSS/css_PAGE_1.css"/>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 
     <title> Shop Theo & Yassine </title>
@@ -31,29 +30,26 @@ else{
 <body>
 
 <div class="header">
+    <a href="#">
+        <img class="logo" src="logoBricolage.png" alt=" LOGO  ">
+    </a>
 
-	<a href="#">
-		<img class="logo" src="logoBricolage.png" alt=" LOGO  ">
-	</a>
-
-	<div class="banniere">
-	  <h1> Shop Theo & Yassine </h1>
-	</div>
-    <a><?php echo $row["name"]; ?></a>
+    <div class="banniere">
+        <h1> Shop Theo & Yassine </h1>
+    </div>
+    <div id="purchase_in" class="menu">
+        <a href="0_PAGES/login.php">CONNEXION</a>
+    </div>
     <div id="out" class="menu">
         <a href="0_PAGES/deco.php">DECONNEXION</a>
     </div>
-
-	<div id="purchase_in" class="menu">
-		<a href="0_PAGES/login.php">CONNEXION</a>
-	</div>
-	<div class="purchase_border">
-	</div>
+    <div class="purchase_border">
+    </div>
 
 
-	<div id="modeles" class="menu">
-		<a href="0_PAGES/panier.php">PANIER</a>
-	</div>
+    <div id="modeles" class="menu">
+        <a href="0_PAGES/panier.php">PANIER</a>
+    </div>
 
 
 </div>
@@ -62,25 +58,25 @@ else{
 <div class="espaceVideo">
 
 
-	<video width="100%" loop="true" muted="true" autoplay="true" >
-  		<source src="brico.mp4" type="video/mp4" />
-  	</video>
+    <video width="100%" loop="true" muted="true" autoplay="true">
+        <source src="brico.mp4" type="video/mp4"/>
+    </video>
 
-	<div class="bio">
+    <div class="bio">
 
         <div id="modeles" class="menu">
             <a href="0_PAGES/modeles.php">NOS PRODUITS</a>
         </div>
 
-	  <h1> Du choix a porter de main ! </h1>
-		<p>Un choix parmis de nombreuses pièces à porter de mains juste pour vous !</p>
-	</div>
+        <h1> Du choix a porter de main ! </h1>
+        <p>Un choix parmis de nombreuses pièces à porter de mains juste pour vous !</p>
+    </div>
 </div>
 
 
 <div class="espaceDiapo">
 
-	<div class="Boxdiapo">
+    <div class="Boxdiapo">
         <div class="slides">
             <div><img class="diapo" src="2_IMAGES/DIAPO/bague.jpg"/></div>
             <div><img class="diapo" src="2_IMAGES/DIAPO/boulonPivot.jpg"/></div>
@@ -88,29 +84,36 @@ else{
             <div><img class="diapo" src="2_IMAGES/DIAPO/clavette.jpg"/></div>
             <div><img class="diapo" src="2_IMAGES/DIAPO/crochet.jpg"/></div>
         </div>
-    </div> 
+    </div>
 
     <h1> Notre savoir faire mise a contribution</h1>
 
-	<p>Une qualité de produits certifiés Française ! avec un service après vente à votre disposition pour régler n'importe quel problème.Acheter des pièces automobiles de haute qualité, c'est beaucoup plus que rajouter simplement quelque chose dans votre panier. Il faut vous assurer que les pièces que vous commandez persistent dans le temps. Vous pouvez être sûr que chaque élément choisi pour votre véhicule sera soigneusement sélectionné.
+    <p>Une qualité de produits certifiés Française ! avec un service après vente à votre disposition pour régler
+        n'importe quel problème.Acheter des pièces automobiles de haute qualité, c'est beaucoup plus que rajouter
+        simplement quelque chose dans votre panier. Il faut vous assurer que les pièces que vous commandez persistent
+        dans le temps. Vous pouvez être sûr que chaque élément choisi pour votre véhicule sera soigneusement
+        sélectionné.
 
-		Lorsque vous faites confiance à Taros Trade, vous serez convaincu que notre équipe travaillera avec vous pour votre entière satisfaction à chaque étape du processus.
+        Lorsque vous faites confiance à Taros Trade, vous serez convaincu que notre équipe travaillera avec vous pour
+        votre entière satisfaction à chaque étape du processus.
 
-		Notre politique c’est la vraie transparence et communication auprès nos clients. Cela signifie que vous aurez accès à notre équipe multilingue de spécialistes passionnés des pièces automobiles, 7 jours sur 7, sans exception.
+        Notre politique c’est la vraie transparence et communication auprès nos clients. Cela signifie que vous aurez
+        accès à notre équipe multilingue de spécialistes passionnés des pièces automobiles, 7 jours sur 7, sans
+        exception.
 
-		Testez-nous!</p>
+        Testez-nous!</p>
 
 
 </div>
 <!-- -------------------------------------------------------------- -->
 
 <div class="footer">
-	<ul>
-  		<li>LebegueKaddouri©2022</li>
-  		<li><a href="0_PAGES/conseils.pdf">Conseils d'utilisation</a></li>
-		<li><a href="0_PAGES/panier.php">Panier</a></li>
-		<li><a href="0_PAGES/modeles.php">Produits</a></li>
-	</ul>
+    <ul>
+        <li>LebegueKaddouri©2022</li>
+        <li><a href="0_PAGES/conseils.pdf">Conseils d'utilisation</a></li>
+        <li><a href="0_PAGES/panier.php">Panier</a></li>
+        <li><a href="0_PAGES/modeles.php">Produits</a></li>
+    </ul>
 </div>
 </body>
 </html>
